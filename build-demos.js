52d92ac657e49cef47d72cb5d5ddef4ec91704e1
clqu <curosbow@gmail.com>
2024-08-03 21:06:17 +0300

    Update build-demos.js

--- build-demos.js
+++ build-demos.js
@@ -1,5 +1,21 @@
 const fs = require('fs');
-
 const demos = fs.readdirSync('./src/components/demos');
 
-console.log(demos);
+demos.forEach(file => {
+    const content = fs.readFileSync(`./src/components/demos/${file}`, 'utf8');
+    if (!content.includes('// @end-example') && !content.includes('//@end-example')) return;
+
+    const spliter = content.includes('//@end-example') ? '//@end-example' : '// @end-example';
+    const parts = content.split(spliter);
+
+    if (content.includes('// @start-demo-string') || content.includes('//@start-demo-string')) {
+        const sp = content.includes('//@start-demo-string') ? '//@start-demo-string' : '// @start-demo-string';
+        const newContent = content.split(sp)[0] + sp + '\nexport const DemoString = `' + parts[0].replace(/`/g, '\`') + '`;';
+        fs.writeFileSync(`./src/components/demos/${file}`, newContent);
+        console.log(`[!] ${file} has been updated.`);
+    } else {
+        const newContent = content + '\n\n// @start-demo-string\nexport const DemoString = `' + parts[0].replace(/`/g, '\`') + '`;';
+        fs.writeFileSync(`./src/components/demos/${file}`, newContent);
+        console.log(`[!] ${file} has been updated. (NEW FILE)`);
+    };
+});
