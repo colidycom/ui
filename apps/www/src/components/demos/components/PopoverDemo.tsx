f6d138a5e2939f9703cbf54933e0791b41364f14
clqu <curosbow@gmail.com>
2024-08-24 14:11:22 +0300

    Refactor PopoverDemo component for improved accessibility and styling consistency

--- apps/www/src/components/demos/components/PopoverDemo.tsx
+++ apps/www/src/components/demos/components/PopoverDemo.tsx
@@ -1,8 +1,11 @@
 'use client';
 import {
 	Popover,
+	PopoverBody,
 	PopoverClose,
 	PopoverContent,
+	PopoverHeader,
+	PopoverTitle,
 	PopoverTrigger,
 } from '@/colidy-ui/Popover';
 import { Label } from '@/colidy-ui/Label';
@@ -16,64 +19,48 @@ export const Demo = () => {
 	const [maxHeight, setMaxHeight] = useState('none');
 
 	return (
-		<div className="max-w-lg">
+		<main className="max-w-lg">
 			<Popover>
 				<PopoverTrigger>
-					<button className="btn">Open Popover</button>
+					<button className="bg-foreground px-4 py-2 rounded flex justify-center items-center gap-6 w-auto h-12 !outline-none text-primary backdrop-blur">
+						Edit dimensions
+					</button>
 				</PopoverTrigger>
 				<PopoverContent withArrow>
-					<div className="flex justify-between items-center">
-						<h3 className="text-sm">Dimensions</h3>
-						<PopoverClose>
-							<button className="hover:bg-zinc-500/10 transition-colors cursor-pointer w-6 h-6 flex justify-center items-center rounded-sm">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									viewBox="0 0 24 24"
-									width={16}
-									height={16}
-									color={'currentColor'}
-									fill={'none'}
-								>
-									<path
-										d="M19.0005 4.99988L5.00049 18.9999M5.00049 4.99988L19.0005 18.9999"
-										stroke="currentColor"
-										strokeWidth="1.5"
-										strokeLinecap="round"
-										strokeLinejoin="round"
-									/>
-								</svg>
-							</button>
-						</PopoverClose>
-					</div>
-					<div className="grid grid-cols-2 gap-4 items-center mt-6 w-64">
-						<Label label="Width" className="!m-0 !p-0" />
-						<TextField
-							value={width}
-							onChange={e => setWidth(e.target.value)}
-							size="sm"
-						/>
-						<Label label="Max Width" className="!m-0 !p-0" />
-						<TextField
-							value={maxWidth}
-							onChange={e => setMaxWidth(e.target.value)}
-							size="sm"
-						/>
-						<Label label="Height" className="!m-0 !p-0" />
-						<TextField
-							value={height}
-							onChange={e => setHeight(e.target.value)}
-							size="sm"
-						/>
-						<Label label="Max Height" className="!m-0 !p-0" />
-						<TextField
-							value={maxHeight}
-							onChange={e => setMaxHeight(e.target.value)}
-							size="sm"
-						/>
-					</div>
+					<PopoverHeader withClose>
+						<PopoverTitle size="h3">Dimensions</PopoverTitle>
+					</PopoverHeader>
+					<PopoverBody>
+						<div className="grid grid-cols-2 gap-4 items-center w-full">
+							<Label label="Width" className="!m-0 !p-0" />
+							<TextField
+								value={width}
+								onChange={e => setWidth(e.target.value)}
+								size="sm"
+							/>
+							<Label label="Max Width" className="!m-0 !p-0" />
+							<TextField
+								value={maxWidth}
+								onChange={e => setMaxWidth(e.target.value)}
+								size="sm"
+							/>
+							<Label label="Height" className="!m-0 !p-0" />
+							<TextField
+								value={height}
+								onChange={e => setHeight(e.target.value)}
+								size="sm"
+							/>
+							<Label label="Max Height" className="!m-0 !p-0" />
+							<TextField
+								value={maxHeight}
+								onChange={e => setMaxHeight(e.target.value)}
+								size="sm"
+							/>
+						</div>
+					</PopoverBody>
 				</PopoverContent>
 			</Popover>
-		</div>
+		</main>
 	);
 };
 
